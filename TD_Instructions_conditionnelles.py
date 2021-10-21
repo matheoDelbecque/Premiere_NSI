16eee1161fd39923adf39a206e4a71e03448bd74
matheoDelbecque <91486201+matheoDelbecque@users.noreply.github.com>
2021-10-21 11:48:33 +0200

    Update TD_Instructions_conditionnelles.py

--- TD_Instructions_conditionnelles.py
+++ TD_Instructions_conditionnelles.py
@@ -67,3 +67,21 @@ def valeur_absolue(a):
     else:
         return(-a)
 print(valeur_absolue(-9))
+
+#exercice 4
+
+def bowling(boule1, boule2):
+    """
+    Description de la fonction : Affiche à l'écran les performances d'un joueur de bowling
+    boule1 (int) : nombre de quilles renversées avec la première boule
+    boule2 (int) : nombre de quilles renversées avec la deuxième boule
+    return (None) :
+    préconditions sur les entrées : boule1 + boule2 <= 10 
+    """
+    if boule1 == 10:
+        print("X")
+    elif boule1 + boule2 == 10:
+        print("/")
+    else:
+        print(boule1 + boule2)
+bowling(2,6)
