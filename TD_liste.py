f4be4bc057b518395584ffa5fe92fb9c072b1f20
matheoDelbecque <91486201+matheoDelbecque@users.noreply.github.com>
2022-01-06 12:54:30 +0100

    Add files via upload

---
+++ TD_liste.py
@@ -0,0 +1,14 @@
+#exercice 2
+
+help(list.insert)
+
+liste = ['Alice', 'Bob', 'Tom']
+liste.insert(2,'Marc')
+print(liste)
+
+#exercice 3
+
+t = []
+for i in range(0,10,2):
+    t.append(i**2)
+print(t)
