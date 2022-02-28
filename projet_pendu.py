30863aea15f3a36b30d6abeb1f1933c002aa55be
matheoDelbecque <91486201+matheoDelbecque@users.noreply.github.com>
2022-02-28 17:53:38 +0100

    Update projet_pendu.py

--- projet_pendu.py
+++ projet_pendu.py
@@ -1,5 +1,6 @@
 from random import randint
-LETTRE_DEJA_CHOISIES = []
+SCORE = 8
+LETTRES_DEJA_CHOISIES = []
 
 def nettoyage_mot(mot):
     mot = mot.lower()
@@ -17,15 +18,15 @@ def tirage_au_sort(nom_fichier):
     for chiffre in range(randint(1,22274)):
         chaine = fichier.readline()
     fichier.close()
-    return chaine
+    return nettoyage_mot(chaine),'-'*len(nettoyage_mot(chaine))
 
 def deja_choisie(lettre):
     global LETTRES_DEJA_CHOISIES
-    if lettre in LETTRE_DEJA_CHOISIES:
+    if lettre in LETTRES_DEJA_CHOISIES:
         print("lettre déjà choisie")
         return True
     else:
-        LETTRE_DEJA_CHOISIES = LETTRE_DEJA_CHOISIES + [lettre]
+        LETTRES_DEJA_CHOISIES = LETTRES_DEJA_CHOISIES + [lettre]
         return False
 
 def jeu_fini():
@@ -40,7 +41,7 @@ def jeu_fini():
 def afficher_bilan():
     global SCORE
     global MOT_A_DECOUVRIR
-    if score == 0:
+    if SCORE == 0:
         print("Vous avez perdu !!\nLe mot recherché était",MOT_A_DECOUVRIR)
     else:
         print("Bravo! Vous avez trouvé le mot",MOT_A_DECOUVRIR,"\nVotre SCORE est de",SCORE)
@@ -48,10 +49,34 @@ def afficher_bilan():
 def remplace_tiret(lettre):
     global MOT_MYSTERE
     global MOT_A_DECOUVRIR
+    global SCORE
     for i in range(len(MOT_A_DECOUVRIR)):
         if lettre == MOT_A_DECOUVRIR[i]:
-           MOT_MYSTERE = MOT_MYSTERE[0:i] + lettre + MOT_MYSTERE[i+1:len(MOT_MYSTERE)+1]
+           MOT_MYSTERE = MOT_MYSTERE[0:i] + lettre + MOT_MYSTERE[i+1:len(MOT_MYSTERE)]
            resultat = True
         else:
            resultat = False
-    return
+    if lettre in MOT_A_DECOUVRIR:
+        pass
+    else:
+        SCORE = SCORE - 1
+    return resultat
+
+def jouer():
+    global MOT_A_DECOUVRIR
+    global MOT_MYSTERE
+    global SCORE
+    global LETTRES_DEJA_CHOISIES
+    res = tirage_au_sort('dictionnaire.txt')
+    MOT_A_DECOUVRIR = res[0]
+    MOT_MYSTERE = res[1]
+    print("-------------------------\nBIENVENUE AU JEU DU PENDU\n-------------------------")
+    while jeu_fini() == False:
+        print("\nMot à découvrir :", MOT_MYSTERE)
+        lettre = input("proposez une lettre : ")
+        deja_choisie(lettre)
+        remplace_tiret(lettre)
+        print()
+    afficher_bilan()
+
+jouer()
