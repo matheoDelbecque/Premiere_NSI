400f79718fd46d4128d129ee4060e937aa8dd8fa
matheoDelbecque <91486201+matheoDelbecque@users.noreply.github.com>
2022-04-23 11:49:31 +0200

    Update TD_tests.py

--- TD_tests.py
+++ TD_tests.py
@@ -37,3 +37,81 @@ def appartient(v,t):
 if __name__ == '__main__':
     import doctest
     doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)
+
+#exercice 4
+#1)
+#V1
+def est_croissant(t):
+    """
+    >>> est_croissant([9,5,4])
+    False
+    """
+    i = len(t) - 1
+    while i >= 0:
+        if t[i] <= t[i+1] :
+            return True
+        else :
+            return False
+        i = i - 1
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)
+
+#V3
+def est_croissant(t):
+    """
+    >>> est_croissant([1,2,3])
+    True
+    """
+    i = len(t) - 1
+    while i >= 0:
+        if t[i-1] > t[i] :
+            return False
+        i = i - 1
+    return True
+print(est_croissant([1,2,3]))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)
+
+#V4
+def est_croissant(t):
+    """
+    >>> est_croissant([])
+    False
+    """
+    i = len(t) - 1
+    while i > 0:
+        if t[i-1] > t[i] :
+            return False
+        i = i - 1
+    return True
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)
+
+#2)
+#V2
+def est_croissant(t):
+    """
+    >>> est_croissant([9,5,4])
+    False
+    >>> est_croissant([1,2,3])
+    True
+    >>> est_croissant([])
+    False
+    """
+    i = len(t) - 1
+    while i >= 0:
+        if t[i-1] <= t[i] :
+            return True
+        else :
+            return False
+        i = i - 1
+        
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS, verbose=False)
